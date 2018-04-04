bf79529a458f9c85989deb8579809a6ff16840d8
hainet <20400616+hainet@users.noreply.github.com>
2018-04-04 20:20:54 +0900

    GraphQL Servletが利用するObjectMapperを定義

---
+++ src/main/java/com/hainet/graphql/spring/boot/sample/config/AppObjectMapperConfigurer.java
@@ -0,0 +1,15 @@
+package com.hainet.graphql.spring.boot.sample.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import graphql.servlet.ObjectMapperConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppObjectMapperConfigurer implements ObjectMapperConfigurer {
+
+    @Override
+    public void configure(final ObjectMapper mapper) {
+        mapper.registerModule(new JavaTimeModule());
+    }
+}
