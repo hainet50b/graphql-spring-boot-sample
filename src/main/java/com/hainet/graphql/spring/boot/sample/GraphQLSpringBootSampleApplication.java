75b5e3133dd572a440ea0476ec3d075af658797e
hainet <20400616+hainet@users.noreply.github.com>
2018-04-05 09:14:00 +0900

    Polish

--- src/main/java/com/hainet/graphql/spring/boot/sample/GraphQLSpringBootSampleApplication.java
+++ src/main/java/com/hainet/graphql/spring/boot/sample/GraphQLSpringBootSampleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlSpringBootSampleApplication {
+public class GraphQLSpringBootSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GraphqlSpringBootSampleApplication.class, args);
+        SpringApplication.run(GraphQLSpringBootSampleApplication.class, args);
     }
 }
--- src/test/java/com/hainet/graphql/spring/boot/sample/GraphQLSpringBootSampleApplicationTests.java
+++ src/test/java/com/hainet/graphql/spring/boot/sample/GraphQLSpringBootSampleApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GraphqlSpringBootSampleApplicationTests {
+public class GraphQLSpringBootSampleApplicationTests {
 
     @Test
     public void contextLoads() {
