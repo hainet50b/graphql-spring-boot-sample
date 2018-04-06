9f7bf65330baef1c5cbcac7208542091bf6eb89a
hainet <20400616+hainet@users.noreply.github.com>
2018-04-06 19:14:29 +0900

    Use GeneratedKeyHolder instead of issuing SQL(SELECT MAX(id) FROM credit_card).

--- src/main/java/com/hainet/graphql/spring/boot/sample/domain/dao/CreditCardDao.java
+++ src/main/java/com/hainet/graphql/spring/boot/sample/domain/dao/CreditCardDao.java
@@ -4,8 +4,11 @@ import com.hainet.graphql.spring.boot.sample.domain.entity.CreditCard;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -22,17 +25,20 @@ public class CreditCardDao {
     }
 
     public int insert(final CreditCard creditCard) {
-        final int result = this.jdbcTemplate.update(
-                "INSERT INTO credit_card (number, good_thru, brand_id, security_code) VALUES (?, ?, ?, ?)",
-                creditCard.getNumber(),
-                creditCard.getGoodThru(),
-                creditCard.getBrandId(),
-                creditCard.getSecurityCode()
-        );
-        creditCard.setId(this.jdbcTemplate.queryForObject(
-                "SELECT MAX(id) FROM credit_card",
-                Integer.class
-        ));
+        final GeneratedKeyHolder holder = new GeneratedKeyHolder();
+        final int result = this.jdbcTemplate.update(con -> {
+            final PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO credit_card (number, good_thru, brand_id, security_code) VALUES (?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            ps.setString(1, creditCard.getNumber());
+            ps.setObject(2, creditCard.getGoodThru());
+            ps.setInt(3, creditCard.getBrandId());
+            ps.setString(4, creditCard.getSecurityCode());
+
+            return ps;
+        }, holder);
+        creditCard.setId(holder.getKey().intValue());
 
         return result;
     }
