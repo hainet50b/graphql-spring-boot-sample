c223fdd5ae6878cf4cb6ba149d16b390c12f00cb
hainet <20400616+hainet@users.noreply.github.com>
2018-04-12 11:43:16 +0900

    Remove verbose description.

--- src/main/java/com/hainet/graphql/spring/boot/sample/config/GraphQLErrorHandlerConfig.java
+++ src/main/java/com/hainet/graphql/spring/boot/sample/config/GraphQLErrorHandlerConfig.java
@@ -7,7 +7,6 @@ import graphql.servlet.GraphQLErrorHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,39 +15,27 @@ import java.util.stream.Collectors;
 public class GraphQLErrorHandlerConfig implements GraphQLErrorHandler {
 
     @Override
-    public List<GraphQLError> processErrors(final List<GraphQLError> incomingErrors) {
-        incomingErrors.stream()
+    public List<GraphQLError> processErrors(final List<GraphQLError> errors) {
+        errors.stream()
                 .filter(this::isServerError)
                 .forEach(error -> {
                     if (error instanceof Throwable) {
                         log.error("Error executing query!", (Throwable) error);
                     } else {
-                        log.error("Error executing query ({}): {}", error.getClass().getSimpleName(), error.getMessage());
+                        log.error(
+                                "Error executing query ({}): {}",
+                                error.getClass().getSimpleName(),
+                                error.getMessage()
+                        );
                     }
                 });
 
-        final List<GraphQLErrorHolder> serverErrors = incomingErrors.stream()
-                .filter(this::isServerError)
+        return errors.stream()
                 .map(GraphQLErrorHolder::new)
                 .collect(Collectors.toList());
-
-        final List<GraphQLErrorHolder> clientErrors = incomingErrors.stream()
-                .filter(this::isClientError)
-                .map(GraphQLErrorHolder::new)
-                .collect(Collectors.toList());
-
-        final ArrayList<GraphQLError> outgoingErrors = new ArrayList<>();
-        outgoingErrors.addAll(serverErrors);
-        outgoingErrors.addAll(clientErrors);
-
-        return outgoingErrors;
     }
 
     private boolean isServerError(final GraphQLError error) {
         return error instanceof ExceptionWhileDataFetching || error instanceof Throwable;
-    }
-
-    private boolean isClientError(final GraphQLError error) {
-        return !this.isServerError(error);
     }
 }
